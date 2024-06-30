3d9dd99ea372a4126ce182167dcb328dabf7e3dc
shivraj <sk40fs@gmail.com>
2024-06-30 21:11:49 +0530

    url-fixed

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const server = app.listen(PORT, () => {
 });
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
   },
   pingTimeout: 60000,
 });
